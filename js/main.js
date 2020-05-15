9c6587e7273741aa113b3cf75ceeb5b3c961f35a
Matthew VanDyke <matthewvandyke2002@comcast.net>
2020-05-14 23:03:20 -0700

    added VideoGame class and call structure for add new video game

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
+var VideoGame = (function () {
+    function VideoGame() {
+    }
+    return VideoGame;
+}());
 window.onload = function () {
     var addBtn = document.querySelector("input[type=button]");
     addBtn.onclick = addVideoGame;
 };
 function addVideoGame() {
-    alert("test");
+    console.log("Add video game was called");
+    if (isAllDataValid()) {
+        var game = getVideoGame();
+        displayGame(game);
+    }
+}
+function displayGame(myGame) {
+}
+function getVideoGame() {
+    var myGame = new VideoGame;
+    return myGame;
+}
+function isAllDataValid() {
+    return true;
 }
--- ts/main.ts
+++ ts/main.ts
@@ -1,8 +1,43 @@
+class VideoGame{
+    title:string;
+    price:number;
+    rating:string;
+    isDigitalOnly:boolean;
+}
+
+//test code
+/*
+let myGame = new VideoGame();
+myGame.title = "Mario";
+myGame.rating = "E"
+myGame.isDigitalOnly = true;
+*/
+
 window.onload = function(){
     let addBtn:HTMLElement = <HTMLElement>document.querySelector("input[type=button]");
     addBtn.onclick = addVideoGame;
 }
 
 function addVideoGame(){
-    alert("test");
+    console.log("Add video game was called");
+
+    if(isAllDataValid()){
+        let game = getVideoGame();
+        displayGame(game);
+    }
+}
+
+function displayGame(myGame:VideoGame):void{
+    // 
+}
+
+function getVideoGame():VideoGame{
+    // TODO: create game, populate with data, return game
+    let myGame = new VideoGame;
+
+    return myGame;
+}
+
+function isAllDataValid():boolean{
+    return true;
 }
